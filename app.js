64cffbeba82bab2ef64240fb2900afe42f6b5328
Rlss91 <rlss91@gmail.com>
2022-11-24 11:41:10 +0200

    more basic node

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const printFunction = require("./print"); //import
 // const math = require("./math");
 const { PI, add } = require("./math");
 require("./global");
+const chalk = require("chalk");
 
 console.log("hello from node");
 
@@ -14,3 +15,11 @@ console.log(PI);
 console.log(add(1, 1));
 
 console.log("🚀 ~ file: app.js ~ line 17 ~ global.samData", global.samData);
+
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+
+console.log(chalk.red("test"));
