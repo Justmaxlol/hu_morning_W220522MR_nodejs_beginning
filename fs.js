83520b059223e837dda0ef8b1638c21e399ff7fe
Rlss91 <rlss91@gmail.com>
2022-11-24 10:01:36 +0200

    fs

--- fs.js
+++ fs.js
@@ -1,4 +1,11 @@
-const { mkdir, rmdir, rm } = require("node:fs/promises");
+const {
+  mkdir,
+  rmdir,
+  rm,
+  writeFile,
+  readdir,
+  readFile,
+} = require("node:fs/promises");
 const { join } = require("node:path");
 
 //* create directory
@@ -25,14 +32,70 @@ const { join } = require("node:path");
 // })();
 
 //* will remove the directory with it constant
+// (async () => {
+//   try {
+//     const newDir = await rm(join(__dirname, "uploads"), {
+//       force: true,
+//       recursive: true,
+//     });
+//     console.log("🚀 ~ file: fs.js ~ line 15 ~ newDir", newDir);
+//   } catch (err) {
+//     console.log("🚀 ~ file: fs.js ~ line 17 ~ err", err);
+//   }
+// })();
+
+// will create file
+// (async () => {
+//   try {
+//     await writeFile("./text.txt", "something new");
+//   } catch (error) {
+//     console.log("🚀 ~ file: fs.js ~ line 44 ~ error", error);
+//   }
+// })();
+
+//* will remove the file
+//! remove recursive option!!!
+// (async () => {
+//   try {
+//     const newDir = await rm("./test/t2.t");
+//     console.log("🚀 ~ file: fs.js ~ line 15 ~ newDir", newDir);
+//   } catch (err) {
+//     console.log("🚀 ~ file: fs.js ~ line 17 ~ err", err);
+//   }
+// })();
+
+// (async () => {
+//   try {
+//     const files = await readdir("./test");
+//     console.log("🚀 ~ file: fs.js ~ line 70 ~ files", files);
+//   } catch (error) {
+//     console.log("🚀 ~ file: fs.js ~ line 72 ~ error", error);
+//   }
+// })();
+
+//read file
+//if the file is text we shuld add encoding type
+//the common encoding type today is utf8
+// (async () => {
+//   try {
+//     const txt = await readFile("./test/t.t", { encoding: "utf-8" });
+//     console.log("🚀 ~ file: fs.js ~ line 79 ~ txt", txt);
+//   } catch (error) {
+//     console.log("🚀 ~ file: fs.js ~ line 80 ~ error", error);
+//   }
+// })();
+
+/* 
+  1) create file and write hello file
+  2) display all files
+  3) delete the file from (1)
+*/
+
 (async () => {
   try {
-    const newDir = await rm(join(__dirname, "uploads"), {
-      force: true,
-      recursive: true,
-    });
-    console.log("🚀 ~ file: fs.js ~ line 15 ~ newDir", newDir);
-  } catch (err) {
-    console.log("🚀 ~ file: fs.js ~ line 17 ~ err", err);
-  }
+    await writeFile("./test/file.txt", "hello file");
+    const files = await readdir("./test");
+    console.log(files);
+    await rm("./test/file.txt");
+  } catch (error) {}
 })();
